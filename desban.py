58d63489ff41fb7d10152f9d85046febacb31399
@Slayerkkk_ <110862456+slayerkk@users.noreply.github.com>
2022-11-27 22:08:11 -0300

    Add files via upload
    clique aqui para usar o script ----> https://cdn.discordapp.com/attachments/1045769921684049930/1046592773056364584/image.png

---
+++ desban.py
@@ -0,0 +1,51 @@
+import smtplib
+import email.message
+
+
+#banner
+print ('███╗░░██╗██╗░░░██╗███╗░░░███╗██████╗░███████╗██████╗░')
+print ('████╗░██║██║░░░██║████╗░████║██╔══██╗██╔════╝██╔══██╗')
+print ('██╔██╗██║██║░░░██║██╔████╔██║██████╦╝█████╗░░██████╔╝')
+print ('██║╚████║██║░░░██║██║╚██╔╝██║██╔══██╗██╔══╝░░██╔══██╗')
+print ('██║░╚███║╚██████╔╝██║░╚═╝░██║██████╦╝███████╗██║░░██║')
+print ('╚═╝░░╚══╝░╚═════╝░╚═╝░░░░░╚═╝╚═════╝░╚══════╝╚═╝░░╚═╝')
+print ('                                                     ')
+print ('██████╗░███████╗░██████╗██████╗░░█████╗░███╗░░██╗')
+print ('██╔══██╗██╔════╝██╔════╝██╔══██╗██╔══██╗████╗░██║')
+print ('██║░░██║█████╗░░╚█████╗░██████╦╝███████║██╔██╗██║')
+print ('██║░░██║██╔══╝░░░╚═══██╗██╔══██╗██╔══██║██║╚████║')
+print ('██████╔╝███████╗██████╔╝██████╦╝██║░░██║██║░╚███║')
+print ('╚═════╝░╚══════╝╚═════╝░╚═════╝░╚═╝░░╚═╝╚═╝░░╚══╝')
+print ('                                                     ')
+
+#banner by
+print ('█▀▀▄ █░░█ 　 █▀▀ █░░ █▀▀█ █░░█ █▀▀ █▀▀█ ')
+print ('█▀▀▄ █▄▄█ 　 ▀▀█ █░░ █▄▄█ █▄▄█ █▀▀ █▄▄▀ ')
+print ('▀▀▀░ ▄▄▄█ 　 ▀▀▀ ▀▀▀ ▀░░▀ ▄▄▄█ ▀▀▀ ▀░▀▀ ')
+
+
+
+#auto
+
+a = input('coloque o numero q vc quer desbanir:')
+
+#email
+def enviar_email():
+    corpo_email = f'Olá suporte do whatsapp, meu numero foi banido de forma injusta!!, por meios ilegais. Solicito o desbanimento do mesmo, o numero é {a}'
+ 
+    msg = email.message.Message()
+    msg['Subject'] = 'Olá suporte do whatsapp!'
+    msg['From'] = 'slayerandkr@gmail.com'
+    msg['To'] = 'support@whatsapp.com'
+    password = 'llqeiynzlzneozlu'
+    msg.add_header ('Content-Type', 'text/html')
+    msg.set_payload(corpo_email )
+#login
+    s = smtplib.SMTP('smtp.gmail.com: 587')
+    s.starttls()
+    # Login Credentials for sending the mail
+    s.login(msg['From'], password)
+    s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
+    print('o numero sera desbanido em breve.')
+
+enviar_email()
